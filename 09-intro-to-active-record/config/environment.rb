e9fda8f187c03eedd6598ea7a39d3432c87620ed
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-06-13 11:29:09 -0400

    prelecture

--- 09-intro-to-active-record/config/environment.rb
+++ 09-intro-to-active-record/config/environment.rb
@@ -1,6 +1,9 @@
 require 'bundler/setup'
 Bundler.require
 
+# setting up the database connection (old way)
+DB = SQLite3::Database.new("db/development.sqlite3")
+
 # ActiveRecord::Base.establish_connection(
 #   adapter: 'sqlite3',
 #   database: "db/development.sqlite3"
--- 09-intro-to-active-record/environment.rb
+++
@@ -1,6 +0,0 @@
-require 'sqlite3'
-require 'require_all'
-require_all 'lib'
-
-# setting up the database connection (old way)
-DB = SQLite3::Database.new("chinook.db")
