992f2710cf2541a6f42cc846887a1e7581b7221c
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-06-22 14:29:26 -0400

    added missing files

--- 12-intro-to-the-internet/app.rb
+++ 12-intro-to-the-internet/app.rb
@@ -3,11 +3,7 @@ require_relative './config/environment.rb'
 class App
 
   def self.call(env)
-    # To use Rack, provide an "app": an object that responds to the call method,
-    # taking the environment hash as a parameter, and returning an Array with three elements:
-        # - The HTTP response code
-        # - A Hash of headers
-        # - The response body, which must respond to each"
+
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
