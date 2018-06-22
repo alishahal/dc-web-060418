cae377d051e007c1733425813c665946c2daa16e
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-06-22 14:26:43 -0400

    didn't mess everything up

--- 12-intro-to-the-internet/app.rb
+++ 12-intro-to-the-internet/app.rb
@@ -1,13 +1,18 @@
-require 'rack'
-require_relative './spicegirl.rb'
+require_relative './config/environment.rb'
 
 class App
 
-  def self.call(environment_hash)
-    
-    req = Rack::Request.new(environment_hash)
+  def self.call(env)
+    # To use Rack, provide an "app": an object that responds to the call method,
+    # taking the environment hash as a parameter, and returning an Array with three elements:
+        # - The HTTP response code
+        # - A Hash of headers
+        # - The response body, which must respond to each"
+    req = Rack::Request.new(env)
     resp = Rack::Response.new
 
-    resp.write("<h1>Hello World</h1>")
+    resp.write("Hello World")
+
+    resp.finish
   end
 end
