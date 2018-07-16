ad095835a08b2273397cfe3573bf17b9580fc42d
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-16 15:45:05 -0400

    renamed folder

---
+++ 23-intro-to-js/script.js
@@ -0,0 +1,36 @@
+console.log("I'm connected")
+
+let obj = {number: 3}
+
+function addToNumber(x){
+  x.number = x.number + 5;
+  return x
+}
+
+console.log("addToNumber:", addToNumber(obj))
+console.log("value of num:", obj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = "hello"
+// alert(a)
+// console.log("More code")
+
+
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   var h1Element = document.querySelector("h1")
+//   console.log(h1Element)
+// })
