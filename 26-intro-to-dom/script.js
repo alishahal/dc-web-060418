fc5875283de53040e3d9865c906d985cbfb09a30
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-18 10:35:16 -0400

    added intro to dom post lecture

--- 26-intro-to-dom/script.js
+++ 26-intro-to-dom/script.js
@@ -1,5 +1,34 @@
-console.log("connected");
+
+function appendNewRowElement(node){
+  let newRow = document.createElement("tr")
+  newRow.id = "row-4"
+
+  let tdFirstName = document.createElement("td")
+  let tdLastName = document.createElement("td")
+  let tdEmail = document.createElement("td")
+
+  newRow.appendChild(tdFirstName)
+  newRow.appendChild(tdLastName)
+  newRow.appendChild(tdEmail)
+
+  tdFirstName.innerText ="Jane"
+  tdLastName.innerText ="Doe"
+  tdEmail.innerText ="Jane@gmail.com"
+
+  node.appendChild(newRow)
+}
+
+function updateInnerHTMLOfElement(node){
+  let str = `<tr id="row-4">
+          <td class="firstName">Jane</td>
+          <td class="lastName">Doe</td>
+          <td class="email">Jane@gmail.com</td>
+        </tr>`
+  node.innerHTML += str
+}
 
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("DOM Loaded");
-});
+  let tbody = document.querySelector("tbody")
+  appendNewRowElement(tbody)
+  updateInnerHTMLOfElement(tbody)
+})
