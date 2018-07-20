c244d3e47cb2378cbd858374dc97d0e2c4954beb
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-20 11:11:43 -0400

    added promises post lecture

---
+++ 28-promises-fetch/README.md
@@ -0,0 +1,25 @@
+## Promises and Fetch
+
+### How do we handle async code?
+- Callbacks to handle async
+
+### What if you have multiple async code that depend on each other?
+- Given the name of the Pokemon, I want to find out the effect/description of its hidden ability
+
+### Callback Hell
+- nested callbacks within nested callbacks
+- triangle shape
+- really hard to read
+
+### Promises and what are they
+- [MDN documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
+- pending
+- fulfilled (resolved/rejected)
+
+### Welcome Fetch
+- built in web api
+- returns a promise object
+
+### Promise chaining
+
+### .then() and .catch()
---
+++ 28-promises-fetch/script.js
@@ -0,0 +1,28 @@
+// getHttp(`https://pokeapi.co/api/v2/pokemon`, function(data){
+//   let URL = //loop through data, find URL
+//   gettHttp(`URL`, function(data){
+//     let anotherURL = //get abilities array, where is_hidden === true
+//     gettHttp(`anotherURL`, function(data){
+//       let description //get effect_entries -> effect
+//       console.log(
+//         let description
+//       )
+//     })
+//   })
+// })
+
+// console.log("tryiing to get pikachu's hiddle ability description")
+// setTimeout(function(){
+//   console.log("getting data for all pokemon")
+//   setTimeout(function(){
+//     console.log("getting data for a single pokemon")
+//     setTimeout(function(){
+//       console.log("get data for a single ability")
+//       console.log("Found a description for the ability!")
+//     }, 1200)
+//   }, 1500)
+// }, 1000)
+
+//dothing1
+//dothing2
+//dothing3
